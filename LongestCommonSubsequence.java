a96684a885bf9537faa0a2fb13b8300cf68c78b9
vinay <vinaygadge2003@gmail.com>
2026-07-26 21:06:03 +0530

    LCS

---
+++ LongestCommonSubsequence.java
@@ -0,0 +1,34 @@
+public class LongestCommonSubsequence {
+    class Solution {
+        public int longestCommonSubsequence(String t1, String t2){
+            int m=t1.length();
+            int n=t2.length();
+            int t[][]=new int[m+1][n+1];
+            for(int i=0;i<=m;i++){
+                for(int j=0;j<=n;j++){
+                    t[i][j]=-1;
+                }
+            }
+            return func(t,t1,t2,m,n);
+
+
+
+
+        }
+        public static int func(int t[][],String t1,String t2,int m,int n){
+            if(m==0||n==0){
+                return 0;  // koi char hai hi nhi to comapare kisko karna
+            }
+            if(t[m][n]!=-1){
+                return t[m][n]; //
+            }
+
+            if(t1.charAt(m-1)==t2.charAt(n-1)){
+                return t[m][n]=1+func(t,t1,t2,m-1,n-1);
+            }
+            else{
+                return t[m][n]=Math.max(func(t,t1,t2,m,n-1),func(t,t1,t2,m-1,n));
+            }
+        }
+    }
+}
