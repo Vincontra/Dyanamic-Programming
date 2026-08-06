e6d927c5a820720d2ec9ca0648512e26dcffa4f7
vinay <vinaygadge2003@gmail.com>
2026-08-07 00:56:52 +0530

    lcs length attempt second

--- LongestCommonSubstring.java
+++ LongestCommonSubstring.java
@@ -34,4 +34,27 @@ public class LongestCommonSubstring {
         }
     }
 
+    class Solution1 {
+        // more optimized one
+        public int longCommSubstr(String s1, String s2) {
+            int m=s1.length();
+            int n=s2.length();
+            int t[][]=new int[m+1][n+1];
+            int ans=0;
+            for(int i=1;i<=m;i++){
+                for(int j=1;j<=n;j++){
+                    if(s1.charAt(i-1)==s2.charAt(j-1)){
+                        t[i][j]=1+t[i-1][j-1];
+                        ans=Math.max(ans,t[i][j]);
+                    }else{
+                        t[i][j]=0;
+                    }
+                }
+            }
+            return ans;
+
+        }
+
+    }
+
 }
